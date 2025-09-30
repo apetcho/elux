6b5bdf30417e89e8b465cd9921a96827c8e508e1
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2025-09-30 18:53:31 +0100

    Implementat of Number::ceil() method

--- src/core.cpp
+++ src/core.cpp
@@ -572,8 +572,15 @@ Number Number::floor(void) const{
     return Number(std::floor(this->as_float()));
 }
 
+// -*-
+Number Number::ceil(void) const{
+    if(this->is_complex()){
+        throw Error(Error::Kind::TypeError, "`ceil` is not supported for complex numbers");
+    }
+    return Number(std::ceil(this->as_float()));
+}
+
 /*
-Number Number::ceil(void) const{}
 Number Number::round(void) const{}
 Number Number::log(void) const{}
 Number Number::log2(void) const{}
