b0905e5c73522f45de1e539d6177b994f4e8409e
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2025-09-30 18:50:07 +0100

    Implementat of Number::max() method

--- src/core.cpp
+++ src/core.cpp
@@ -545,8 +545,26 @@ Number Number::min(const Number& other) const{
     return Number(std::min(x, y));
 }
 
+// -*-
+Number Number::max(const Number& other) const{
+    if(this->is_complex() || other.is_complex()){
+        auto _ty1 = this->type().str();
+        auto _ty2 = other.type().str();
+        std::stringstream ss;
+        ss << "`max' cannot be applied between `" << _ty1 << "' and `";
+        ss << _ty2 << "' objects";
+        throw Error(Error::Kind::TypeError, ss.str());
+    }else if(this->is_integer() || other.is_integer()){
+        auto x = this->as_integer();
+        auto y = other.as_integer();
+        return Number(std::max(x, y));
+    }
+    auto x = this->as_float();
+    auto y = other.as_float();
+    return Number(std::max(x, y));
+}
+
 /*
-Number Number::max(const Number& other) const{}
 Number Number::floor(void) const{}
 Number Number::ceil(void) const{}
 Number Number::round(void) const{}
