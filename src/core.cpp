c3fa448b98ff9f46f1d58643957fe1985d77c3bc
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2025-09-30 20:22:08 +0100

    Implementat of Number::asinh() method

--- src/core.cpp
+++ src/core.cpp
@@ -855,8 +855,15 @@ Number Number::tanh(void) const{
     return Number(std::tanh(this->as_float()));
 }
 
+// -*-
+Number Number::asinh(void) const{
+    if(this->is_complex()){
+        return Number(std::asinh(this->as_complex()));
+    }
+    return Number(std::asinh(this->as_float()));
+}
+
 /*
-Number Number::asinh(void) const{}
 Number Number::acosh(void) const{}
 Number Number::atanh(void) const{}
 Number Number::erf(void) const{}
