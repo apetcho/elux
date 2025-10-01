e274b9b1f27496a4f217627545c69a9331d61de0
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2025-10-01 15:45:30 +0100

    Implement klx::share(const std::list<Self>&) function

--- src/core.cpp
+++ src/core.cpp
@@ -1890,8 +1890,12 @@ Self share(const Vec<Self>& xs){
     return std::make_shared<List>(xs);
 }
 
+// -*-
+Self share(const std::list<Self>& xs){
+    return std::make_shared<List>(xs);
+}
+
 /*
-Self share(const std::list<Self>& xs){}
 Self share(const Str& name, CFun fun, i64 minArgc, i64 maxArgc){}
 Self share(const Vec<Symbol>& params, const Vec<Self>& body, const Env& env){}
 Self share(const Str& name, const Vec<Symbol>& params, const Vec<Self>& body, const Env& env, bool macro){}
