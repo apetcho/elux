ffcb87458d4c0951ba2ea53f3713686e26f00263
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2025-10-03 23:15:12 +0100

    Update Lynx::fn_list_remove() method

--- src/lynx.cpp
+++ src/lynx.cpp
@@ -3396,6 +3396,41 @@ Result Lynx::fn_list_insert(const Vec<Self>& args){
     return Result(share());
 }
 
+// -*-
+Result Lynx::fn_list_remove(const Vec<Self>& args){
+    //! @todo: add doc-string of `list.remove' to lynxDocs describing it syntax
+    /*
+        (list.remove xs idx)
+    */
+    Error err;
+    auto argc = args.size();
+    auto pred = (argc==2);
+    if(!Lynx::check_argc(pred, "list.remove", err)){
+        return Result(std::move(err));
+    }
+    if(!Lynx::check_type(args[0]->is_list(), args[0], err)){
+        err.message() += "\nExpect the first argument to `list.remove' to be a list";
+        return Result(std::move(err));
+    }
+    if(!Lynx::check_type(args[1]->is_integer(), args[1], err)){
+        err.message() += "\nExpect the second argument to `list.remove' to be an integer.";
+        return Result(std::move(err));
+    }
+    
+    auto xs = *dynamic_cast<List*>(args[0].get());
+    try{
+        auto num = *dynamic_cast<Number*>(args[1].get());
+        auto idx = num.as_integer();
+        auto ans = xs.remove(idx).as_list();
+        return Result(share(ans));
+    }catch(const Error& err_){
+        err = err_;
+        return Result(std::move(err));
+    }
+    return Result(share());
+}
+
+
 /*
 //! @todo: add doc-string of `cond' to lynxDocs describing it syntax
 
@@ -3411,7 +3446,6 @@ Result Lynx::fn_list_insert(const Vec<Self>& args){
 // Result Lynx::fn_take(const Vec<Self>& args){}
 // Result Lynx::fn_take_while(const Vec<Self>& args){}
 
-Result Lynx::fn_list_remove(const Vec<Self>& args){}
 Result Lynx::fn_list_push(const Vec<Self>& args){}
 Result Lynx::fn_list_pop(const Vec<Self>& args){}
 Result Lynx::fn_list_append(const Vec<Self>& args){}
