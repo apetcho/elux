c594fb691c182613c290854630de365147514263
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2025-10-03 20:52:35 +0100

    Implement Lynx::fn_lt() method

--- src/lynx.cpp
+++ src/lynx.cpp
@@ -2673,7 +2673,24 @@ Result Lynx::fn_mod(const Vec<Self>& args){
         return Result(std::move(err));
     }
     auto ans = xnum % ynum;
-    
+
+    return Result(share(ans));
+}
+
+// --------------------------------------------------------------------
+// -*-                   Relational operators                       -*-
+// --------------------------------------------------------------------
+// -*-
+Result Lynx::fn_lt(const Vec<Self>& args){
+    //! @todo: add doc-string of `<' to lynxDocs describing it syntax
+    Error err;
+    auto argc = args.size();
+    auto pred = (argc==2);
+    if(!Lynx::check_argc(pred, "<", err)){
+        return Result(std::move(err));
+    }
+    auto ans = (args[0] < args[1]);
+
     return Result(share(ans));
 }
 
@@ -2689,9 +2706,6 @@ Result Lynx::fn_mod(const Vec<Self>& args){
     auto xs = *dynamic_cast<List*>(self.get());
     auto vec = xs.as_vector();
 
-
-// Relational operators
-Result Lynx::fn_lt(const Vec<Self>& args){;}
 Result Lynx::fn_le(const Vec<Self>& args){}
 Result Lynx::fn_gt(const Vec<Self>& args){}
 Result Lynx::fn_ge(const Vec<Self>& args){}
