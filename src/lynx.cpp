38f2697f859c5ec4cdedaac098a6ae0d7f939d03
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2025-10-03 19:14:49 +0100

    Implement Lynx::fn_is_number() method

--- src/lynx.cpp
+++ src/lynx.cpp
@@ -2186,6 +2186,19 @@ Result Lynx::fn_is_bool(const Vec<Self>& args){
     return Result(share(self->is_bool()));
 }
 
+// -*-
+Result Lynx::fn_is_number(const Vec<Self>& args){
+    //! @todo: add doc-string of `number?' to lynxDocs describing it syntax
+    Error err;
+    auto argc = args.size();
+    auto pred = (argc==1);
+    if(!Lynx::check_argc(pred, "number?", err)){
+        return Result(std::move(err));
+    }
+    auto self = args[0];
+    return Result(share(self->is_number()));
+}
+
 /*
 //! @todo: add doc-string of `cond' to lynxDocs describing it syntax
 
@@ -2198,7 +2211,6 @@ Result Lynx::fn_is_bool(const Vec<Self>& args){
     auto xs = *dynamic_cast<List*>(self.get());
     auto vec = xs.as_vector();
 
-Result Lynx::fn_is_number(const Vec<Self>& args){}
 Result Lynx::fn_is_integer(const Vec<Self>& args){}
 Result Lynx::fn_is_float(const Vec<Self>& args){}
 Result Lynx::fn_is_complex(const Vec<Self>& args){}
