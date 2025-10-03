43e6f5f6c6db314289b8d2b31d5956b02ddd0495
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2025-10-03 23:18:51 +0100

    Update Lynx::fn_list_push() method

--- src/lynx.cpp
+++ src/lynx.cpp
@@ -3430,6 +3430,28 @@ Result Lynx::fn_list_remove(const Vec<Self>& args){
     return Result(share());
 }
 
+// -*-
+Result Lynx::fn_list_push(const Vec<Self>& args){
+    //! @todo: add doc-string of `list.push' to lynxDocs describing it syntax
+    /*
+        (list.push xs val)
+    */
+    Error err;
+    auto argc = args.size();
+    auto pred = (argc==2);
+    if(!Lynx::check_argc(pred, "list.push", err)){
+        return Result(std::move(err));
+    }
+    if(!Lynx::check_type(args[0]->is_list(), args[0], err)){
+        err.message() += "\nExpect the first argument to `list.push' to be a list";
+        return Result(std::move(err));
+    }
+    
+    auto xs = *dynamic_cast<List*>(args[0].get());
+    auto ans = xs.push(args[1]).as_list();
+    return Result(share(ans));
+}
+
 
 /*
 //! @todo: add doc-string of `cond' to lynxDocs describing it syntax
@@ -3446,7 +3468,6 @@ Result Lynx::fn_list_remove(const Vec<Self>& args){
 // Result Lynx::fn_take(const Vec<Self>& args){}
 // Result Lynx::fn_take_while(const Vec<Self>& args){}
 
-Result Lynx::fn_list_push(const Vec<Self>& args){}
 Result Lynx::fn_list_pop(const Vec<Self>& args){}
 Result Lynx::fn_list_append(const Vec<Self>& args){}
 Result Lynx::fn_list_set(const Vec<Self>& args){}
