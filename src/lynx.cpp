79d4195565b03802d4359c8d737ac6e0efb060a4
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2025-10-04 10:24:51 +0100

    Implement Lynx::fn_complex_imag() method

--- src/lynx.cpp
+++ src/lynx.cpp
@@ -4857,6 +4857,34 @@ Result Lynx::fn_complex_real(const Vec<Self>& args){
     return Result(share(num));
 }
 
+// -*-
+Result Lynx::fn_complex_imag(const Vec<Self>& args){
+    //! @todo: add doc-string of `math::complex.imag' to lynxDocs describing it syntax
+    /*
+        (math::complex.imag x)
+    */
+    Error err;
+    auto argc = args.size();
+    auto pred = (argc==1);
+    if(!Lynx::check_argc(pred, "math::complex.imag", err)){
+        return Result(std::move(err));
+    }
+    if(!Lynx::check_type(args[0]->is_number(), args[0], err)){
+        err.message() += "\nExpect the argument of `math::complex.imag' to be a number";
+        return Result(std::move(err));
+    }
+    
+    Number num{};
+    try{
+        num = *dynamic_cast<Number*>(args[0].get());
+        num = num.imag();
+    }catch(const Error& err_){
+        err = err_;
+        return Result(std::move(err));
+    }
+    return Result(share(num));
+}
+
 
 /*
 //! @todo: add doc-string of `cond' to lynxDocs describing it syntax
@@ -4873,7 +4901,7 @@ Result Lynx::fn_complex_real(const Vec<Self>& args){
 // Result Lynx::fn_take(const Vec<Self>& args){}
 // Result Lynx::fn_take_while(const Vec<Self>& args){}
 
-Result Lynx::fn_complex_imag(const Vec<Self>& args){}
+
 Result Lynx::fn_complex_arg(const Vec<Self>& args){}
 Result Lynx::fn_complex_norm(const Vec<Self>& args){}
 Result Lynx::fn_complex_conj(const Vec<Self>& args){}
