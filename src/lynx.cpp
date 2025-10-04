0ff1bdd64d50d7573ab4630ba697d28bc173187e
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2025-10-04 10:20:58 +0100

    Implement Lynx::fn_isnan() method

--- src/lynx.cpp
+++ src/lynx.cpp
@@ -4801,6 +4801,34 @@ Result Lynx::fn_isinf(const Vec<Self>& args){
     return Result(share(ans));
 }
 
+// -*-
+Result Lynx::fn_isnan(const Vec<Self>& args){
+    //! @todo: add doc-string of `math::isnan?' to lynxDocs describing it syntax
+    /*
+        (math::isnan? x)
+    */
+    Error err;
+    auto argc = args.size();
+    auto pred = (argc==1);
+    if(!Lynx::check_argc(pred, "math::isnan?", err)){
+        return Result(std::move(err));
+    }
+    if(!Lynx::check_type(args[0]->is_number(), args[0], err)){
+        err.message() += "\nExpect the argument of `math::isnan?' to be a number";
+        return Result(std::move(err));
+    }
+    
+    bool ans{};
+    try{
+        auto num = *dynamic_cast<Number*>(args[0].get());
+        ans = num.isnan();
+    }catch(const Error& err_){
+        err = err_;
+        return Result(std::move(err));
+    }
+    return Result(share(ans));
+}
+
 /*
 //! @todo: add doc-string of `cond' to lynxDocs describing it syntax
 
@@ -4816,7 +4844,7 @@ Result Lynx::fn_isinf(const Vec<Self>& args){
 // Result Lynx::fn_take(const Vec<Self>& args){}
 // Result Lynx::fn_take_while(const Vec<Self>& args){}
 
-Result Lynx::fn_isnan(const Vec<Self>& args){}
+
 Result Lynx::fn_complex_real(const Vec<Self>& args);
 Result Lynx::fn_complex_imag(const Vec<Self>& args);
 Result Lynx::fn_complex_arg(const Vec<Self>& args);
