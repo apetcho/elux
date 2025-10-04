5bfd3b9e8effb549a70788495cdb7fccb80667d1
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2025-10-04 03:39:01 +0100

    Implement Lynx::fn_asin() method

--- src/lynx.cpp
+++ src/lynx.cpp
@@ -4513,6 +4513,34 @@ Result Lynx::fn_tan(const Vec<Self>& args){
     return Result(share(num));
 }
 
+// -*-
+Result Lynx::fn_asin(const Vec<Self>& args){
+    //! @todo: add doc-string of `math::asin' to lynxDocs describing it syntax
+    /*
+        (math::asin x)
+    */
+    Error err;
+    auto argc = args.size();
+    auto pred = (argc==1);
+    if(!Lynx::check_argc(pred, "math::asin", err)){
+        return Result(std::move(err));
+    }
+    if(!Lynx::check_type(args[0]->is_number(), args[0], err)){
+        err.message() += "\nExpect the argument of `math::asin' to be a number";
+        return Result(std::move(err));
+    }
+
+    Number num{};
+    try{
+        num = *dynamic_cast<Number*>(args[0].get());
+        num = num.asin();
+    }catch(const Error& err_){
+        err = err_;
+        return Result(std::move(err));
+    }
+    return Result(share(num));
+}
+
 /*
 //! @todo: add doc-string of `cond' to lynxDocs describing it syntax
 
@@ -4528,7 +4556,7 @@ Result Lynx::fn_tan(const Vec<Self>& args){
 // Result Lynx::fn_take(const Vec<Self>& args){}
 // Result Lynx::fn_take_while(const Vec<Self>& args){}
 
-Result Lynx::fn_asin(const Vec<Self>& args){}
+
 Result Lynx::fn_acos(const Vec<Self>& args){}
 Result Lynx::fn_atan(const Vec<Self>& args){}
 Result Lynx::fn_atan2(const Vec<Self>& args){}
