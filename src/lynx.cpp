a5e840449a26b9c6454feb386b4c75b6f2228955
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2025-10-04 03:11:39 +0100

    Implement Lynx::fn_log2() method

--- src/lynx.cpp
+++ src/lynx.cpp
@@ -4171,6 +4171,34 @@ Result Lynx::fn_log(const Vec<Self>& args){
     return Result(share(num));
 }
 
+// -*-
+Result Lynx::fn_log2(const Vec<Self>& args){
+    //! @todo: add doc-string of `math::log2' to lynxDocs describing it syntax
+    /*
+        (math::log2 x)
+    */
+    Error err;
+    auto argc = args.size();
+    auto pred = (argc==1);
+    if(!Lynx::check_argc(pred, "math::log2", err)){
+        return Result(std::move(err));
+    }
+    if(!Lynx::check_type(args[0]->is_number(), args[0], err)){
+        err.message() += "\nExpect the argument of `math::log2' to be a number";
+        return Result(std::move(err));
+    }
+
+    Number num{};
+    try{
+        num = *dynamic_cast<Number*>(args[0].get());
+        num = num.log2();
+    }catch(const Error& err_){
+        err = err_;
+        return Result(std::move(err));
+    }
+    return Result(share(num));
+}
+
 /*
 //! @todo: add doc-string of `cond' to lynxDocs describing it syntax
 
@@ -4187,8 +4215,6 @@ Result Lynx::fn_log(const Vec<Self>& args){
 // Result Lynx::fn_take_while(const Vec<Self>& args){}
 
 
-
-Result Lynx::fn_log2(const Vec<Self>& args){}
 Result Lynx::fn_log10(const Vec<Self>& args){}
 Result Lynx::fn_log1p(const Vec<Self>& args){}
 Result Lynx::fn_exp(const Vec<Self>& args){}
