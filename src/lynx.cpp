84f7b6dc73be95b81c3c76b4b932d1e1ca0d5505
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2025-10-04 10:19:24 +0100

    Implement Lynx::fn_isinf() method

--- src/lynx.cpp
+++ src/lynx.cpp
@@ -4773,6 +4773,34 @@ Result Lynx::fn_isfinite(const Vec<Self>& args){
     return Result(share(ans));
 }
 
+// -*-
+Result Lynx::fn_isinf(const Vec<Self>& args){
+    //! @todo: add doc-string of `math::isinf?' to lynxDocs describing it syntax
+    /*
+        (math::isinf? x)
+    */
+    Error err;
+    auto argc = args.size();
+    auto pred = (argc==1);
+    if(!Lynx::check_argc(pred, "math::isinf?", err)){
+        return Result(std::move(err));
+    }
+    if(!Lynx::check_type(args[0]->is_number(), args[0], err)){
+        err.message() += "\nExpect the argument of `math::isinf?' to be a number";
+        return Result(std::move(err));
+    }
+    
+    bool ans{};
+    try{
+        auto num = *dynamic_cast<Number*>(args[0].get());
+        ans = num.isinf();
+    }catch(const Error& err_){
+        err = err_;
+        return Result(std::move(err));
+    }
+    return Result(share(ans));
+}
+
 /*
 //! @todo: add doc-string of `cond' to lynxDocs describing it syntax
 
@@ -4788,7 +4816,6 @@ Result Lynx::fn_isfinite(const Vec<Self>& args){
 // Result Lynx::fn_take(const Vec<Self>& args){}
 // Result Lynx::fn_take_while(const Vec<Self>& args){}
 
-Result Lynx::fn_isinf(const Vec<Self>& args){}
 Result Lynx::fn_isnan(const Vec<Self>& args){}
 Result Lynx::fn_complex_real(const Vec<Self>& args);
 Result Lynx::fn_complex_imag(const Vec<Self>& args);
