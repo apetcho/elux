7520fcff8df83817d1894e6b8a9ac63e0a6f989c
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2025-10-04 03:27:54 +0100

    Implement Lynx::fn_sqrt() method

--- src/lynx.cpp
+++ src/lynx.cpp
@@ -4373,6 +4373,34 @@ Result Lynx::fn_pow(const Vec<Self>& args){
     return Result(share(num));
 }
 
+// -*-
+Result Lynx::fn_sqrt(const Vec<Self>& args){
+    //! @todo: add doc-string of `math::sqrt' to lynxDocs describing it syntax
+    /*
+        (math::sqrt x)
+    */
+    Error err;
+    auto argc = args.size();
+    auto pred = (argc==1);
+    if(!Lynx::check_argc(pred, "math::sqrt", err)){
+        return Result(std::move(err));
+    }
+    if(!Lynx::check_type(args[0]->is_number(), args[0], err)){
+        err.message() += "\nExpect the argument of `math::sqrt' to be a number";
+        return Result(std::move(err));
+    }
+
+    Number num{};
+    try{
+        num = *dynamic_cast<Number*>(args[0].get());
+        num = num.sqrt();
+    }catch(const Error& err_){
+        err = err_;
+        return Result(std::move(err));
+    }
+    return Result(share(num));
+}
+
 /*
 //! @todo: add doc-string of `cond' to lynxDocs describing it syntax
 
@@ -4389,7 +4417,6 @@ Result Lynx::fn_pow(const Vec<Self>& args){
 // Result Lynx::fn_take_while(const Vec<Self>& args){}
 
 
-Result Lynx::fn_sqrt(const Vec<Self>& args){}
 Result Lynx::fn_cbrt(const Vec<Self>& args){}
 Result Lynx::fn_sin(const Vec<Self>& args){}
 Result Lynx::fn_cos(const Vec<Self>& args){}
