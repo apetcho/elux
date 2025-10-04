30e698d228bda09c50ba92b7bcc5e368d0460a31
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2025-10-04 10:13:56 +0100

    Implement Lynx::fn_tgamma() method

--- src/lynx.cpp
+++ src/lynx.cpp
@@ -4688,6 +4688,34 @@ Result Lynx::fn_erfc(const Vec<Self>& args){
     return Result(share(num));
 }
 
+// -*-
+Result Lynx::fn_tgamma(const Vec<Self>& args){
+    //! @todo: add doc-string of `math::tgamma' to lynxDocs describing it syntax
+    /*
+        (math::tgamma x)
+    */
+    Error err;
+    auto argc = args.size();
+    auto pred = (argc==1);
+    if(!Lynx::check_argc(pred, "math::tgamma", err)){
+        return Result(std::move(err));
+    }
+    if(!Lynx::check_type(args[0]->is_number(), args[0], err)){
+        err.message() += "\nExpect the argument of `math::tgamma' to be a number";
+        return Result(std::move(err));
+    }
+    
+
+    Number num{};
+    try{
+        num = *dynamic_cast<Number*>(args[0].get());
+        num = num.tgamma();
+    }catch(const Error& err_){
+        err = err_;
+        return Result(std::move(err));
+    }
+    return Result(share(num));
+}
 
 
 /*
@@ -4705,7 +4733,6 @@ Result Lynx::fn_erfc(const Vec<Self>& args){
 // Result Lynx::fn_take(const Vec<Self>& args){}
 // Result Lynx::fn_take_while(const Vec<Self>& args){}
 
-Result Lynx::fn_tgamma(const Vec<Self>& args){}
 Result Lynx::fn_lgamma(const Vec<Self>& args){}
 Result Lynx::fn_isfinite(const Vec<Self>& args){}
 Result Lynx::fn_isinf(const Vec<Self>& args){}
