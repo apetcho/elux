219aad45b916240e9229539d6c77e7230b6395da
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2026-03-28 13:58:36 +0100

    Implement ekasoft::klx::ELux::collect(Iterator,  List&) method

--- src/elux.cpp
+++ src/elux.cpp
@@ -545,8 +545,12 @@ void ELux::collect(Iterator iter, Array& result){
     iter->collect(result);
 }
 
+// -*-
+void ELux::collect(Iterator iter, List& result){
+    iter->collect(result);
+}
+
 /*
-void ELux::collect(Iterator iter, List& result){}
 void ELux::collect(Iterator iter, Set& result){}
 void ELux::collect(Iterator iter, Dict& result){}
 */
