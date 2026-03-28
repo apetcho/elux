342faa8070228730b6007fd4a6055d3cb00ec4ee
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2026-03-28 02:42:12 +0100

    Implement ekasoft::klx::Number::isinf() method

--- src/elux-core.cpp
+++ src/elux-core.cpp
@@ -241,9 +241,13 @@ bool Number::isnan(void) const{
     return std::isnan(this->as_float());
 }
 
+// -*-
+bool Number::isinf(void) const{
+    return std::isinf(this->as_float());
+}
+
 /*
 // -*-
-bool Number::isinf(void) const{}
 bool Number::isfinite(void) const{}
 Number& Number::operator!(){}
 Number& Number::operator-(){}
