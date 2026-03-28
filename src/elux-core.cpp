ce0e62ef5eb84de8f03714814aba4380da1600ca
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2026-03-28 19:39:31 +0100

    Implement ekasoft::klx::operator<(...) method

--- src/elux-core.cpp
+++ src/elux-core.cpp
@@ -2207,7 +2207,7 @@ bool operator>=(const Self& lhs, const Self& rhs){
     if(ELux::is_number(lhs) && ELux::is_number(rhs)){
         auto xnum = *dynamic_cast<Number*>(lhs.get());
         auto ynum = *dynamic_cast<Number*>(rhs.get());
-        return (xnum<=ynum);
+        return (xnum>=ynum);
     }
     if(lhs->type()==rhs->type() && ELux::is_string(lhs)){
         auto xstr = *dynamic_cast<String*>(lhs.get());
@@ -2219,8 +2219,25 @@ bool operator>=(const Self& lhs, const Self& rhs){
     throw ELuxError(ELuxError::TypeError, ss.str());
 }
 
+// -*-
+bool operator<(const Self& lhs, const Self& rhs){
+    if(ELux::is_number(lhs) && ELux::is_number(rhs)){
+        auto xnum = *dynamic_cast<Number*>(lhs.get());
+        auto ynum = *dynamic_cast<Number*>(rhs.get());
+        return (xnum < ynum);
+    }
+    if(lhs->type()==rhs->type() && ELux::is_string(lhs)){
+        auto xstr = *dynamic_cast<String*>(lhs.get());
+        return (xstr.compare(rhs.get()) < 0);
+    }
+
+    std::stringstream ss;
+    ss << "`<' is not support for type " << std::quoted(lhs->type());
+    throw ELuxError(ELuxError::TypeError, ss.str());
+}
+
 /*
-bool operator<(const Self& lhs, const Self& rhs){}
+
 bool operator>(const Self& lhs, const Self& rhs){}
 Self operator+(const Self& lhs, const Self& rhs){}
 Self operator-(const Self& lhs, const Self& rhs){}
