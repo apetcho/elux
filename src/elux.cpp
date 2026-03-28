1b50b9afb9ba39477b2d05b9bd5d952a23a53117
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2026-03-28 18:41:58 +0100

    Implement ekasoft::klx::ELux::check_runtime() method

--- src/elux.cpp
+++ src/elux.cpp
@@ -619,8 +619,14 @@ void ELux::check_syntax(bool pred, const std::string& message){
     }
 }
 
+// -*-
+void ELux::check_runtime(bool pred, const std::string& message){
+    if(!pred){
+        throw ELuxError(ELuxError::RuntimeError, message);
+    }
+}
+
 /*
-void ELux::check_runtime(bool pred, const std::string& message){}
 void ELux::check_key(bool pred, const std::string& message){}
 void ELux::check_index(bool pred, const std::string& message){}
 void ELux::check(bool pred, const std::string& message){}
